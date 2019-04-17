43329d25aab37a72dcd1725cc5e7ab5dfd2ade6b
ChenPeiRong <jk43268@yahoo.com.tw>
2019-04-18 07:13:52 +0800

    //Fix-J-month error

--- app/src/main/java/com/example/nccumis/add_expense.java
+++ app/src/main/java/com/example/nccumis/add_expense.java
@@ -155,9 +155,10 @@ public class add_expense extends AppCompatActivity {
         DatePickerDialog datePickerDialog = new DatePickerDialog(add_expense.this, new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                monthOfYear++;
                 add_expense.this.input_date.setText(year + "年" + monthOfYear + "月" + dayOfMonth+"日");
             }
-        }, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
+        }, calendar.get(Calendar.YEAR),calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
         datePickerDialog.show();
     }
 
