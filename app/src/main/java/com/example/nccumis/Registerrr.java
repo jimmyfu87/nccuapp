7e6d145c54d759cdbb48cdd2e95e197ec0877f77
NatashaTsai <105306061@nccu.edu.tw>
2019-04-26 01:07:22 +0800

    add"Registerrr" isEmpty

--- app/src/main/java/com/example/nccumis/Registerrr.java
+++ app/src/main/java/com/example/nccumis/Registerrr.java
@@ -2,6 +2,8 @@ package com.example.nccumis;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.text.method.HideReturnsTransformationMethod;
 import android.text.method.PasswordTransformationMethod;
 import android.view.View;
@@ -12,21 +14,42 @@ import android.widget.EditText;
 
 public class Registerrr extends AppCompatActivity {
 
-    private EditText et_userName = (EditText)findViewById(R.id.et_userName);
-    private EditText et_userAccount = (EditText)findViewById(R.id.et_userAccount);
-    private EditText et_userPhone = (EditText)findViewById(R.id.et_userPhone);
-    private EditText et_userEmail = (EditText)findViewById(R.id.et_userEmail);
-    private EditText et_userPassword1 = (EditText)findViewById(R.id.et_userPassword1);
-    private EditText et_userPassword2 = (EditText)findViewById(R.id.et_userPassword2);
-    private EditText et_userBirth = (EditText)findViewById(R.id.et_userBirth);
-    private Button btn_checkRegister = (Button) findViewById(R.id.btn_checkRegister);
-    private CheckBox checkBoxPassword1 = (CheckBox) findViewById(R.id.checkBoxPassword1);
-    private CheckBox checkBoxPassword2 = (CheckBox) findViewById(R.id.checkBoxPassword2);
+    private EditText et_userName ;
+    private EditText et_userAccount ;
+    private EditText et_userPhone ;
+    private EditText et_userEmail ;
+    private EditText et_userPassword1 ;
+    private EditText et_userPassword2 ;
+    private EditText et_userBirth ;
+    private Button btn_checkRegister ;
+    private CheckBox checkBoxPassword1 ;
+    private CheckBox checkBoxPassword2 ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registerrr);
+
+         et_userName = findViewById(R.id.et_userName);
+         et_userAccount = findViewById(R.id.et_userAccount);
+         et_userPhone = findViewById(R.id.et_userPhone);
+         et_userEmail = findViewById(R.id.et_userEmail);
+         et_userPassword1 = findViewById(R.id.et_userPassword1);
+         et_userPassword2 = findViewById(R.id.et_userPassword2);
+         et_userBirth = findViewById(R.id.et_userBirth);
+         btn_checkRegister =  findViewById(R.id.btn_checkRegister);
+         checkBoxPassword1 = findViewById(R.id.checkBoxPassword1);
+         checkBoxPassword2 =  findViewById(R.id.checkBoxPassword2);
+
+         et_userName.addTextChangedListener(registerrrTextWatcher );
+        et_userAccount.addTextChangedListener(registerrrTextWatcher );
+        et_userPhone.addTextChangedListener(registerrrTextWatcher );
+        et_userEmail.addTextChangedListener(registerrrTextWatcher );
+        et_userPassword1.addTextChangedListener(registerrrTextWatcher );
+        et_userPassword2.addTextChangedListener(registerrrTextWatcher );
+        et_userBirth.addTextChangedListener(registerrrTextWatcher );
+
+
 
         checkBoxPassword1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
@@ -48,25 +71,32 @@ public class Registerrr extends AppCompatActivity {
                 }
             }
         });
-
-        btn_checkRegister.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if()
-            }
-        });
-
-    }
-
-   public boolean isInputEmpty (EditText editText){
-        if(editText.getText().toString().isEmpty()){
-            editText.setError("不能為空白");
-            return false;
-        }else{
-            return true;
-        }
-
    }
+   private TextWatcher registerrrTextWatcher = new TextWatcher() {
+       @Override
+       public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+       }
+
+       @Override
+       public void onTextChanged(CharSequence s, int start, int before, int count) {
+           String usernameInput =et_userName.getText().toString().trim();
+           String userAccountInput =et_userAccount.getText().toString().trim();
+           String userEmailInput =et_userEmail.getText().toString().trim();
+           String userPassword1Input =et_userPassword1.getText().toString().trim();
+           String userPassword2Input =et_userPassword2.getText().toString().trim();
+           String userPhoneInput =et_userPhone.getText().toString().trim();
+           String userBirthInput =et_userBirth.getText().toString().trim();
+
+           btn_checkRegister.setEnabled(!usernameInput.isEmpty()&&!userAccountInput.isEmpty()&&!userEmailInput.isEmpty()&&!userBirthInput.isEmpty()&&!userPassword1Input.isEmpty()&&!userPassword2Input.isEmpty()&&!userPhoneInput.isEmpty());
+
+       }
+
+       @Override
+       public void afterTextChanged(Editable s) {
+
+       }
+   };
 
 
     }
