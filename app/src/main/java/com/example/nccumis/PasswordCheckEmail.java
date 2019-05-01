0478c97abaad9399d223092b8855f2cc96981715
NatashaTsai <105306061@nccu.edu.tw>
2019-05-02 00:31:58 +0800

    fix PasswordCheckEmail

--- app/src/main/java/com/example/nccumis/PasswordCheckEmail.java
+++ app/src/main/java/com/example/nccumis/PasswordCheckEmail.java
@@ -3,6 +3,7 @@ package com.example.nccumis;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class PasswordCheckEmail extends AppCompatActivity {
@@ -15,9 +16,16 @@ public class PasswordCheckEmail extends AppCompatActivity {
         setContentView(R.layout.activity_password_check_email);
 
         btn_sendEmail=(Button) findViewById(R.id.btn_sendEmail);
+        btn_sendEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                JumpToLogInn();
+            }
+        });
     }
 
     public void JumpToLogInn(){
-        Intent intent = new Intent();
+        Intent intent = new Intent(PasswordCheckEmail.this.LogInn.class);
+        startActivity(intent);
     }
 }
