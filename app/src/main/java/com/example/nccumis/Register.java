7d81127daa963620199b25e5cdea6dc54b13ae8d
NatashaTsai <105306061@nccu.edu.tw>
2019-05-19 11:57:00 +0800

    fix register

--- app/src/main/java/com/example/nccumis/Register.java
+++ app/src/main/java/com/example/nccumis/Register.java
@@ -87,38 +87,7 @@ public class Register extends AppCompatActivity {
             }
         });
 
-        public void showDatePickDlg() {
-            Calendar calendar = Calendar.getInstance();
-            DatePickerDialog datePickerDialog = new DatePickerDialog(Register.this, new DatePickerDialog.OnDateSetListener() {
-                @Override
-                public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-                    monthOfYear++;
-                    setdateformat(year,monthOfYear,dayOfMonth);
-                    Register.this.et_userBirth.setText(year + "年" + monthOfYear + "月" + dayOfMonth+"日");
 
-                }
-            }, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
-            datePickerDialog.show();
-        }
-        public void setdateformat(int year,int month,int day){
-            String st_month;
-            String st_day;
-            if(month<10){
-                st_month=Integer.toString(month);
-                st_month="0"+st_month;
-            }
-            else{
-                st_month=Integer.toString(month);
-            }
-            if(day<10){
-                st_day=Integer.toString(day);
-                st_day="0"+st_day;
-            }
-            else{
-                st_day=Integer.toString(day);
-            }
-            BirthDate=year+"-"+st_month+"-"+st_day;
-        }
 
 
 
@@ -145,7 +114,44 @@ public class Register extends AppCompatActivity {
         });
    }
 
-   //核對密碼
+    private void showDatePickDlg() {
+        Calendar calendar = Calendar.getInstance();
+        DatePickerDialog datePickerDialog = new DatePickerDialog(Register.this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                monthOfYear++;
+                setdateformat(year,monthOfYear,dayOfMonth);
+                Register.this.et_userBirth.setText(year + "年" + monthOfYear + "月" + dayOfMonth+"日");
+
+            }
+        }, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
+        datePickerDialog.show();
+
+    }
+
+
+    public void setdateformat(int year,int month,int day){
+        String st_month;
+        String st_day;
+        if(month<10){
+            st_month=Integer.toString(month);
+            st_month="0"+st_month;
+        }
+        else{
+            st_month=Integer.toString(month);
+        }
+        if(day<10){
+            st_day=Integer.toString(day);
+            st_day="0"+st_day;
+        }
+        else{
+            st_day=Integer.toString(day);
+        }
+        BirthDate=year+"-"+st_month+"-"+st_day;
+    }
+
+
+    //核對密碼
 
 
    private View.OnClickListener ClickIntHere = new View.OnClickListener() {
