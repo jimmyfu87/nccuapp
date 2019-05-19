a17dcc92e2de7ac224f0a936352f0742bed772db
jimmyfu87 <jimmyfu87@gmail.com>
2019-05-19 16:12:40 +0800

    Add-J-Google帳號登入

--- app/src/main/java/com/example/nccumis/Home.java
+++ app/src/main/java/com/example/nccumis/Home.java
@@ -1,19 +1,40 @@
 package com.example.nccumis;
 
 import android.content.Intent;
+import android.net.wifi.WifiConfiguration;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
 public class Home extends AppCompatActivity {
     private Button addSpend;
     private Button checkSpend;
+    private Button signout;
+    int RC_SIGN_IN=0;
+    GoogleSignInClient mGoogleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
+
+        signout=(Button)findViewById(R.id.signout);
+        signout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signOut();
+            }
+
+        });
 
         //到記帳
         addSpend = (Button) findViewById(R.id.addSpend);
@@ -45,5 +66,20 @@ public class Home extends AppCompatActivity {
         Intent intent = new Intent(Home.this,check_expense.class);
         startActivity(intent);
     }
+    private void signOut() {
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        mGoogleSignInClient.signOut()
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        startActivity(new Intent(Home.this,LogIn.class));
+                        // ...
+                    }
+                });
+    }
+
 
 }
--- app/src/main/java/com/example/nccumis/LogIn.java
+++ app/src/main/java/com/example/nccumis/LogIn.java
@@ -5,11 +5,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.HideReturnsTransformationMethod;
 import android.text.method.PasswordTransformationMethod;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 
 public class LogIn extends AppCompatActivity {
 
@@ -18,11 +28,27 @@ public class LogIn extends AppCompatActivity {
     private Button btn_forgetPassword;
     private Button btn_registerAgain;
     private Button loginHome;
+    int RC_SIGN_IN=0;
+    SignInButton signInButton;
+    GoogleSignInClient mGoogleSignInClient;
 
-    @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        signInButton=findViewById(R.id.sign_in_button);
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn();
+            }
+        });
 
         checkboxLogIn = (CheckBox)findViewById(R.id.checkLogIn);
         et_passwordLogin =(EditText)findViewById(R.id.et_passwordLogin);
@@ -70,6 +96,14 @@ public class LogIn extends AppCompatActivity {
         });
 
     }
+//    protected void onStart(){
+//        super.onStart();
+//        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+//        if(account!=null){
+//            startActivity(new Intent(LogIn.this,Home.class));
+//        }
+//
+//    }
 //切換到驗證密碼的頁面
     public void JumpToPasswordCheckEmail(){
         Intent intent= new Intent(LogIn.this,PasswordCheckEmail.class);
@@ -89,5 +123,45 @@ public class LogIn extends AppCompatActivity {
     public void jumpToHome(){
         Intent intent = new Intent(LogIn.this,Home.class);
         startActivity(intent);
+    }
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            String personName = account.getDisplayName();
+            String personGivenName = account.getGivenName();
+            String personFamilyName = account.getFamilyName();
+            String personEmail = account.getEmail();
+            String personId = account.getId();
+           // Uri personPhoto = account.getPhotoUrl();
+            System.out.println(personName);
+            System.out.println(personGivenName);
+            System.out.println(personFamilyName);
+            System.out.println(personEmail);
+            System.out.println(personId);
+            // Signed in successfully, show authenticated UI.
+            startActivity(new Intent(LogIn.this,Home.class));
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w("Google signin error", "signInResult:failed code=" + e.getStatusCode());
+            Toast.makeText(LogIn.this,"登錄失敗",Toast.LENGTH_LONG).show();
+        }
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
     }
 }
