73c7099ea18641c3e7ebcaf2150bc7f33d0d411d
ChenPeiRong <jk43268@yahoo.com.tw>
2019-05-23 18:52:00 +0800

    Rm-J-Unuse import

--- app/src/main/java/com/example/nccumis/add_expense.java
+++ app/src/main/java/com/example/nccumis/add_expense.java
@@ -1,11 +1,9 @@
 package com.example.nccumis;
 
-import android.app.backup.BackupManager;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.InputType;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
