20f0fe0f34e31ce4dc62ea44b3c63435185889cd
ChenPeiRong <jk43268@yahoo.com.tw>
2019-06-08 18:01:51 +0800

    Rf-J-move merchandise , onlineShopping to new package

--- app/src/main/java/com/example/nccumis/com/example/nccumis/onlineshopping/OnlineShopping.java
+++ app/src/main/java/com/example/nccumis/com/example/nccumis/onlineshopping/OnlineShopping.java
@@ -1,10 +1,12 @@
-package com.example.nccumis;
+package com.example.nccumis.com.example.nccumis.onlineshopping;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+
+import com.example.nccumis.R;
 
 
 public class OnlineShopping extends AppCompatActivity {
