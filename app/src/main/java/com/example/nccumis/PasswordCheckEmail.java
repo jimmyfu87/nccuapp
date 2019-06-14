51283d543195efe63a1abb765b79a1b25331419d
jimmyfu87 <jimmyfu87@gmail.com>
2019-06-15 00:21:59 +0800

    Add-J-串接MySQL忘記密碼

--- app/src/main/java/com/example/nccumis/PasswordCheckEmail.java
+++ app/src/main/java/com/example/nccumis/PasswordCheckEmail.java
@@ -1,25 +1,84 @@
 package com.example.nccumis;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class PasswordCheckEmail extends AppCompatActivity {
 
     private Button btn_sendEmail;
+    private EditText et_useremail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_password_check_email);
+        et_useremail=(EditText)findViewById(R.id.Email);
 
         btn_sendEmail=(Button) findViewById(R.id.btn_sendEmail);
         btn_sendEmail.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                JumpToLogInn();
+                final String member_email = et_useremail.getText().toString();
+
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        System.out.println(response);
+                        try {
+                            System.out.println(response);
+                            JSONObject jsonResponse = new JSONObject(response);
+                            String condition = jsonResponse.getString("condition");
+
+                            if (condition .equals("sent")) {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(PasswordCheckEmail.this);
+                                builder.setMessage("信件已寄發")
+                                        .setPositiveButton("知道了", new DialogInterface.OnClickListener() {
+                                            @Override
+                                            public void onClick(DialogInterface dialog, int which) {
+                                                Intent intent = new Intent(PasswordCheckEmail.this, LogIn.class);
+                                                PasswordCheckEmail.this.startActivity(intent);
+                                            }
+                                        })
+                                        .create()
+                                        .show();
+
+                            } else if(condition.equals("wrongformat")) {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(PasswordCheckEmail.this);
+                                builder.setMessage("帳號為非信箱格式，寄發失敗")
+                                        .setPositiveButton("知道了", null)
+                                        .create()
+                                        .show();
+                            }
+                            else  {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(PasswordCheckEmail.this);
+                                builder.setMessage("此帳號未註冊，寄發失敗")
+                                        .setPositiveButton("知道了", null)
+                                        .create()
+                                        .show();
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+                PasswordCheckEmailRequest forgetRequest = new PasswordCheckEmailRequest(member_email, responseListener);
+                RequestQueue queue = Volley.newRequestQueue(PasswordCheckEmail.this);
+                queue.add(forgetRequest);
+                //JumpToLogInn();
             }
         });
     }
---
+++ app/src/main/java/com/example/nccumis/PasswordCheckEmailRequest.java
@@ -0,0 +1,25 @@
+package com.example.nccumis;
+
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PasswordCheckEmailRequest extends StringRequest {
+    private  static String REGISTER_REQUEST_URL="https://nccugo105306.000webhostapp.com/forgetpassword.php";
+    private Map<String,String> params;
+
+    public PasswordCheckEmailRequest(String member_email, Response.Listener<String> listener){
+        super(Method.POST, REGISTER_REQUEST_URL, listener, null);
+        params = new HashMap<>();
+        params.put("member_email", member_email);
+    }
+
+    @Override
+    public Map<String, String> getParams() {
+        return params;
+    }
+
+
+}
