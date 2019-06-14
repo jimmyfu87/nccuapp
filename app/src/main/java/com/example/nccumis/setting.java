1746671331226f4e384a871ccaa40a05300b5a6f
NatashaTsai <105306061@nccu.edu.tw>
2019-06-14 19:02:27 +0800

    add setting

---
+++ app/src/main/java/com/example/nccumis/setting.java
@@ -0,0 +1,14 @@
+package com.example.nccumis;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class setting extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.setting);
+    }
+}
+
