7fbe636987f7ac27994a2ebac1c99adc4723a528
nova1103 <105306021@nccu.edu.tw>
2019-06-15 14:44:21 +0800

    Fix：activity_main(L)、drawermenu(L)、Mainactivity2(J)

--- app/src/main/java/com/example/nccumis/MainActivity2.java
+++ app/src/main/java/com/example/nccumis/MainActivity2.java
@@ -1,5 +1,6 @@
 package com.example.nccumis;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -8,13 +9,17 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity2 extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
-    //
+    private Button addSpend;
+    private Button checkSpend;
     private Toolbar toolbar;
     private DrawerLayout drawerLayout;
     private ActionBarDrawerToggle actionBarDrawerToggle;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -26,14 +31,33 @@ public class MainActivity2 extends AppCompatActivity implements NavigationView.O
         drawerLayout.addDrawerListener(actionBarDrawerToggle);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         NavigationView navigationView = findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);	    }
+        navigationView.setNavigationItemSelectedListener(this);
+        addSpend = (Button) findViewById(R.id.addSpend);
+        addSpend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                jumpToadd_spend();
+            }
+        });
+
+        //到查帳
+        checkSpend = (Button) findViewById(R.id.checkSpend);
+        checkSpend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                jumpTocheck_expense();
+            }
+        });
+
+    }
 
 
     @Override
     public boolean onNavigationItemSelected(MenuItem item){
         int id = item.getItemId();
-        if(id == R.id.homepage){
-            Toast.makeText(this,"首頁",Toast.LENGTH_LONG).show();
+        if(id == R.id.register){
+            Intent intent = new Intent(MainActivity2.this,Register.class);
+            startActivity(intent);
         }
         DrawerLayout drawer = findViewById(R.id.drawer);
         drawer.closeDrawer(GravityCompat.START);
@@ -43,5 +67,14 @@ public class MainActivity2 extends AppCompatActivity implements NavigationView.O
         super.onPostCreate(savedInstanceState);
         actionBarDrawerToggle.syncState();
     }
-}
 
+    public void jumpToadd_spend() {
+        Intent intent = new Intent(MainActivity2.this, add_expense.class);
+        startActivity(intent);
+    }
+
+    public void jumpTocheck_expense() {
+        Intent intent = new Intent(MainActivity2.this, check_expense.class);
+        startActivity(intent);
+    }
+}
