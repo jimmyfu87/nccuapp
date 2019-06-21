6a69e004fb7b6561e52d2efb1d19fcc89c7385cd
NatashaTsai <105306061@nccu.edu.tw>
2019-06-21 16:23:47 +0800

    fix setting

--- app/src/main/java/com/example/nccumis/setting.java
+++ app/src/main/java/com/example/nccumis/setting.java
@@ -2,13 +2,28 @@ package com.example.nccumis;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.EditText;
+
 
 public class setting extends AppCompatActivity {
+
+    private EditText et_predictAmount;
+    private CheckBox ckbox_GLORY;
+    private CheckBox ckbox_Green;
+    private CheckBox ckbox_Wonderful;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.setting);
+
+        et_predictAmount =(EditText)findViewById(R.id.et_predictAmount);
+        ckbox_GLORY =(CheckBox)findViewById(R.id.ckbox_GLORY);
+        ckbox_Green =(CheckBox)findViewById(R.id.ckbox_Green);
+        ckbox_Wonderful=(CheckBox)findViewById(R.id.ckbox_Wonderful);
+
+
     }
 }
 
