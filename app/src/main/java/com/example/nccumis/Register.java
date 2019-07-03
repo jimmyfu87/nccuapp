f71d1c031e2ff0554e432f62eacba82df23b2c71
NatashaTsai <105306061@nccu.edu.tw>
2019-07-04 00:42:39 +0800

    fix 回上頁(跳回新首頁)

--- app/src/main/java/com/example/nccumis/Register.java
+++ app/src/main/java/com/example/nccumis/Register.java
@@ -119,7 +119,7 @@ public class Register extends AppCompatActivity {
         btn_lastPage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                jumpToLogIn();
+                jumpToHome();
             }
         });
    }
@@ -207,8 +207,8 @@ public class Register extends AppCompatActivity {
        }
    };
 
-   public void jumpToLogIn(){
-       startActivity(new Intent(Register.this, LogIn.class));
+   public void jumpToHome(){
+       startActivity(new Intent(Register.this, MainActivity2.class));
    }
 
 
