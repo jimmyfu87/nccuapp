46f3e1cf6111b034dab3e26cf898f00eaa9cb783
NatashaTsai <105306061@nccu.edu.tw>
2019-07-21 00:28:46 +0800

    add BookManage(先上傳給廖用)

--- app/src/main/java/com/example/nccumis/BookManage.java
+++ app/src/main/java/com/example/nccumis/BookManage.java
@@ -2,13 +2,26 @@ package com.example.nccumis;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
+
 
 public class BookManage extends AppCompatActivity {
+
+    private Button btn_showBook;
+    private ListView BookList;
+    private Button btn_newBook;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_management);
+        btn_newBook=(Button)findViewById(R.id.btn_newBook);
+        btn_showBook=(Button)findViewById(R.id.btn_showBook);
+        BookList=(ListView) findViewById(R.id.BookList);
+
+
+
     }
 
 
