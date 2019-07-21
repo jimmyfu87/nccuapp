4d95aa7f8796f15471660c0ed6674fd6ec64a200
nova1103 <105306021@nccu.edu.tw>
2019-07-21 11:08:22 +0800

    add:password_change(J、L)(空格不可空白)

--- app/src/main/java/com/example/nccumis/Register.java
+++ app/src/main/java/com/example/nccumis/Register.java
@@ -269,7 +269,5 @@ public class Register extends AppCompatActivity {
 
 
 
-
-
 }
 
--- app/src/main/java/com/example/nccumis/password_change.java
+++ app/src/main/java/com/example/nccumis/password_change.java
@@ -11,12 +11,15 @@ import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class password_change extends AppCompatActivity {
     private Button btn_lastPage;
+    private Button btn_confirm;
     private EditText ex_password;
     private EditText new_password;
     private EditText check_password;
+    private TextView et_wrongPassword;
 
 
     @Override
@@ -26,10 +29,12 @@ public class password_change extends AppCompatActivity {
         ex_password = findViewById(R.id.ex_password);
         new_password = findViewById(R.id.new_password);
         check_password = findViewById(R.id.check_password);
+        et_wrongPassword = findViewById(R.id.et_wrongPassword);
 
         ex_password.addTextChangedListener(changeTextWatcher);
         new_password.addTextChangedListener(changeTextWatcher);
         check_password.addTextChangedListener(changeTextWatcher);
+        btn_confirm.setOnClickListener(ClickIntHere);
 
 
         btn_lastPage = (Button) findViewById(R.id.btn_lastPage);
@@ -47,6 +52,7 @@ public class password_change extends AppCompatActivity {
 
     }
 
+
     private TextWatcher changeTextWatcher = new TextWatcher() {
         @Override
         public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -55,6 +61,11 @@ public class password_change extends AppCompatActivity {
 
         @Override
         public void onTextChanged(CharSequence s, int start, int before, int count) {
+            String ex_passwordInput = ex_password.getText().toString().trim();
+            String new_passwordInput = new_password.getText().toString().trim();
+            String check_passwordInput = check_password.getText().toString().trim();
+
+            btn_confirm.setEnabled(!ex_passwordInput.isEmpty() && !new_passwordInput.isEmpty() && !check_passwordInput.isEmpty());
 
         }
 
@@ -62,6 +73,25 @@ public class password_change extends AppCompatActivity {
         public void afterTextChanged(Editable s) {
 
         }
+
+    };
+    private View.OnClickListener ClickIntHere = new View.OnClickListener() {
+
+        @Override
+        public void onClick(View v) {
+            final String expassword = ex_password.getText().toString();
+            final String newpassword = new_password.getText().toString();
+            final String checkpassword = check_password.getText().toString();
+
+            if (new_password.getText().toString().isEmpty()) {
+                et_wrongPassword.setText("請輸入新密碼");
+            } else if (new_password.getText().toString().equals(check_password.getText().toString())) {
+                et_wrongPassword.setText("確定變更密碼");
+            }
+
+
+        }
+
 
     };
 }
