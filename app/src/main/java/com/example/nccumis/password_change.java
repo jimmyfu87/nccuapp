8615002c6f9e4a4e647be73abef57d2b5a949455
jimmyfu87 <jimmyfu87@gmail.com>
2019-07-24 21:01:04 +0800

    Add-J-修改密碼串接資料庫

--- app/src/main/java/com/example/nccumis/password_change.java
+++ app/src/main/java/com/example/nccumis/password_change.java
@@ -1,31 +1,46 @@
 package com.example.nccumis;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class password_change<ModifyPswActivity> extends AppCompatActivity {
     private Button btn_lastPage;
     private Button btn_confirm;
-    private EditText ex_password;
     private EditText new_password;
     private EditText check_password;
-    private TextView et_wrongPassword;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_password_change);
-        ex_password = findViewById(R.id.ex_password);
         new_password = findViewById(R.id.new_password);
         check_password = findViewById(R.id.check_password);
-        et_wrongPassword = findViewById(R.id.et_wrongPassword);
+
 
        // ex_password.addTextChangedListener(changeTextWatcher);
        // new_password.addTextChangedListener(changeTextWatcher);
@@ -48,90 +63,71 @@ public class password_change<ModifyPswActivity> extends AppCompatActivity {
         btn_confirm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-
-                if (ex_password.getText().toString().isEmpty() && new_password.getText().toString().isEmpty() && check_password.getText().toString().isEmpty()) {
-                    et_wrongPassword.setText("請趕快填空格");
+                if (new_password.getText().toString().isEmpty() || check_password.getText().toString().isEmpty()) {
+                    Toast.makeText(password_change.this,"請趕快填空格",Toast.LENGTH_SHORT).show();
                 }else if(new_password.getText().toString().isEmpty()){
-                    et_wrongPassword.setText("輸入新密碼");
-                }else if(check_password.getText().toString().isEmpty()) {
-                    et_wrongPassword.setText("確認新密碼");
-                }else if(ex_password.getText().toString().equals("12345") && new_password.getText().toString().isEmpty()){
-                    et_wrongPassword.setText("輸入新密碼");
-                }else if(ex_password.getText().toString().isEmpty() && new_password.getText().toString().equals(check_password.getText().toString())){
-                    et_wrongPassword.setText("請輸入舊密碼");
-               // }else if(ex_password.getText().toString().isEmpty() && new_password.getText().toString().isEmpty() && check_password.getText().toString().isEmpty()) {
-                   // et_wrongPassword.setText("趕快填空格!!!");
-                }else if(ex_password.getText().toString().equals("12345") && !new_password.getText().toString().equals(check_password.getText().toString())){
-                    et_wrongPassword.setText("請盡速確認密碼");
-                }else if(!ex_password.getText().toString().equals("12345") && new_password.getText().toString().equals(check_password.getText().toString())){
-                    et_wrongPassword.setText("舊密碼錯誤");
+                    Toast.makeText(password_change.this,"請輸入新密碼",Toast.LENGTH_SHORT).show();
                 }
-                else if (ex_password.getText().toString().equals("12345") && new_password.getText().toString().equals(check_password.getText().toString())) {
-                    Intent intent = new Intent(password_change.this, Settings.class);
-                    startActivity(intent);
-                    Toast.makeText(password_change.this,"密碼已更新",Toast.LENGTH_SHORT).show();
+                else if (new_password.getText().toString().equals(check_password.getText().toString())) {
+                    SharedPreferences sp = getSharedPreferences("User", MODE_PRIVATE);
+                    SharedPreferences.Editor editor = sp.edit();
+                    String member_id=sp.getString("member_id",null);
+                    Response.Listener<String> responseListener = new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            try {
+                                JSONObject jsonResponse = new JSONObject(response);
+                                boolean success = jsonResponse.getBoolean("success");
+                                if (success) {
+                                    AlertDialog.Builder builder = new AlertDialog.Builder(password_change.this);
+                                    builder.setMessage("密碼更新成功")
+                                            .setPositiveButton("知道了", new DialogInterface.OnClickListener() {
+                                                @Override
+                                                public void onClick(DialogInterface dialog, int which) {
+                                                    Intent intent = new Intent(password_change.this, Settings.class);
+                                                    startActivity(intent);
+                                                }
+                                            })
+                                            .create()
+                                            .show();
+
+                                } else {
+                                    AlertDialog.Builder builder = new AlertDialog.Builder(password_change.this);
+                                    builder.setMessage("密碼更新失敗")
+                                            .setPositiveButton("知道了", null)
+                                            .create()
+                                            .show();
+                                }
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    };
+                    String newpassword=new_password.getText().toString();
+                    ChangepasswordRequest changepasswordRequest = new ChangepasswordRequest(member_id, newpassword,responseListener);
+                    RequestQueue queue = Volley.newRequestQueue(password_change.this);
+                    queue.add(changepasswordRequest);
+                }
+                else{
+                    Toast.makeText(password_change.this,"密碼不一致",Toast.LENGTH_SHORT).show();
                 }
 
             }
-
-
-
         });
-
-
+    }
+    public class ChangepasswordRequest extends StringRequest {
+        private static final String Changepassword_REQUEST_URL="https://nccugo105306.000webhostapp.com/changepassword.php";
+        private Map<String,String> params;
+        public ChangepasswordRequest(String member_id,String newpassword, Response.Listener<String> listener){
+            super(Method.POST, Changepassword_REQUEST_URL, listener, null);
+            params = new HashMap<>();
+            params.put("member_id", member_id);
+            params.put("newpassword", newpassword);
+        }
+        @Override
+        public Map<String, String> getParams() {
+            return params;
+        }
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-    //private TextWatcher changeTextWatcher = new TextWatcher() {
-       // @Override
-       // public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-       // }
-
-        //@Override
-       // public void onTextChanged(CharSequence s, int start, int before, int count) {
-        //    String ex_passwordInput = ex_password.getText().toString().trim();
-        //    String new_passwordInput = new_password.getText().toString().trim();
-        //    String check_passwordInput = check_password.getText().toString().trim();
-
-        //    btn_confirm.setEnabled(!ex_passwordInput.isEmpty() && !new_passwordInput.isEmpty() && !check_passwordInput.isEmpty());
-
-       // }
-
-       // @Override
-       // public void afterTextChanged(Editable s) {
-
-       // }
-
-   // };
-   // private View.OnClickListener ClickIntHere = new View.OnClickListener() {
-
-        //@Override
-        //public void onClick(View v) {
-          //  final String expassword = ex_password.getText().toString();
-         //   final String newpassword = new_password.getText().toString();
-         //   final String checkpassword = check_password.getText().toString();
-
-         //   if (new_password.getText().toString().isEmpty()) {
-         //       et_wrongPassword.setText("請輸入新密碼");
-          //  } else if (new_password.getText().toString().equals(check_password.getText().toString())) {
-          //      et_wrongPassword.setText("確定變更密碼");
-          //  }
-
-
-      //  }
-   // };
     }
