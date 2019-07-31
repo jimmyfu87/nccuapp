06ab2f98c0117175ce625b5755302786de4756f4
nova1103 <105306021@nccu.edu.tw>
2019-07-31 21:32:52 +0800

    fix:BookManage(J)(修改程式碼，未完成)

--- app/src/main/java/com/example/nccumis/BookManage.java
+++ app/src/main/java/com/example/nccumis/BookManage.java
@@ -11,13 +11,18 @@ import android.widget.ArrayAdapter;
 import android.widget.AdapterView;
 import android.widget.ListAdapter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class BookManage extends AppCompatActivity {
 
     private Button btn_showBook;
     private ListView BookList;
     private Button btn_newBook;
-
+    private ArrayList<String> selectBooks = new ArrayList<String>();
+    private ArrayList<String> bookArray = new ArrayList<String>();
+    private List<Book> select_book = new ArrayList<Book>();
     @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,19 +30,34 @@ public class BookManage extends AppCompatActivity {
         setContentView(R.layout.activity_book_management);
         btn_newBook=(Button)findViewById(R.id.btn_newBook);
         btn_showBook=(Button)findViewById(R.id.btn_showBook);
-
         BookList=(ListView) findViewById(R.id.BookList);
 
 
-        btn_showBook.setOnClickListener(new View.OnClickListener() {
+        this.btn_showBook =(Button)findViewById(R.id.btn_newBook);
+        btn_newBook.setOnClickListener(new View.OnClickListener() {
+
+
             @Override
             public void onClick(View v) {
-                shoebook();
+                DatabaseManager dbmanager=new DatabaseManager(getApplicationContext());
+                dbmanager.open();
+                BookList = (ListView) dbmanager.fetchBook();
+                dbmanager.close();
+                setbookname();
+                setList();
+
             }
 
 
 
+
         });
+
+
+
+
+
+
         btn_newBook.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -53,30 +73,13 @@ public class BookManage extends AppCompatActivity {
         });
 
     }
-    public void shoebook() {
-        ListView listView;
-        String[] values = new String[]{
-                "美金帳本",
-                "旅遊帳本",
-                "支票帳本",
-        };
 
-        //protected void onCreate(Bundle savedInstanceState) {
-        //super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_book_management);
-        listView = (ListView) findViewById(R.id.BookList);
-        ListAdapter adapter = new ArrayAdapter<>(this , android.R.layout.simple_list_item_1 ,values);
-        listView.setAdapter(adapter);
-        listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
-        //設定選擇的模式
-
-
+    private void setbookname() {
+        
     }
-    
 
-
-
-
+    private void setList() {
+    }
 
 
 }
