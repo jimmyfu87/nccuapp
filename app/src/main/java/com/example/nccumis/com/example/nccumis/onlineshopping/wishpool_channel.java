2c2b0fe794a31db95aa2cc52a14fdef92b83ce70
ChenPeiRong <jk43268@yahoo.com.tw>
2019-08-21 17:08:44 +0800

    FIX-J-wishpool bug

--- app/src/main/java/com/example/nccumis/com/example/nccumis/onlineshopping/wishpool_channel.java
+++ app/src/main/java/com/example/nccumis/com/example/nccumis/onlineshopping/wishpool_channel.java
@@ -9,7 +9,6 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListAdapter;
 import android.widget.ListView;
