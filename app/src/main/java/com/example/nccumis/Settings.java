6b5ae636eb17495bf3fbd4b15bf6f60146db169e
jimmyfu87 <jimmyfu87@gmail.com>
2019-09-08 15:31:43 +0800

    Add-J-Setting新增和刪除卡片

--- app/src/main/java/com/example/nccumis/Settings.java
+++ app/src/main/java/com/example/nccumis/Settings.java
@@ -3,6 +3,7 @@ package com.example.nccumis;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -48,6 +49,8 @@ public class Settings extends AppCompatActivity {
     private List<Cardtype> owncardtype_list=new ArrayList<Cardtype>();
     private List<Cardtype> othercardtype_list=new ArrayList<Cardtype>();
     private Button password_change;
+    private Button addcardrelation;
+    private Button deletecardrelation;
 
 
 
@@ -137,6 +140,93 @@ public class Settings extends AppCompatActivity {
         GetothercardRequest getRequest2 = new GetothercardRequest(sp2.getString("member_id",null),responseListener2);
         RequestQueue requestQueue2 = Volley.newRequestQueue(getApplicationContext());
         requestQueue2.add(getRequest2);
+
+        addcardrelation=(Button)findViewById(R.id.addcardrelation);
+        addcardrelation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Response.Listener<String> responseListener3 = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonResponse = new JSONObject(response);
+                            boolean success = jsonResponse.getBoolean("success");
+                            if (success) {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(Settings.this);
+                                builder.setMessage("新增成功")
+                                        .setPositiveButton("知道了", new DialogInterface.OnClickListener() {
+                                            @Override
+                                            public void onClick(DialogInterface dialog, int which) {
+                                            }
+                                        })
+                                        .create()
+                                        .show();
+
+                            } else {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(Settings.this);
+                                builder.setMessage("新增失敗")
+                                        .setPositiveButton("知道了", null)
+                                        .create()
+                                        .show();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+                //cardtype_name要放入新增的卡名
+                String cardtype_name="i-Fun愛玩樂卡";
+                SharedPreferences sp3 = getSharedPreferences("User", MODE_PRIVATE);
+                SharedPreferences.Editor editor3 = sp3.edit();
+                AddcardrelationRequest getRequest3 = new AddcardrelationRequest(sp3.getString("member_id",null),cardtype_name,responseListener3);
+                RequestQueue requestQueue3 = Volley.newRequestQueue(getApplicationContext());
+                requestQueue3.add(getRequest3);
+
+            }
+        });
+        deletecardrelation=(Button)findViewById(R.id.deletecardrelation);
+        deletecardrelation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Response.Listener<String> responseListener4 = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonResponse = new JSONObject(response);
+                            boolean success = jsonResponse.getBoolean("success");
+                            if (success) {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(Settings.this);
+                                builder.setMessage("刪除成功")
+                                        .setPositiveButton("知道了", new DialogInterface.OnClickListener() {
+                                            @Override
+                                            public void onClick(DialogInterface dialog, int which) {
+                                            }
+                                        })
+                                        .create()
+                                        .show();
+
+                            } else {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(Settings.this);
+                                builder.setMessage("刪除失敗")
+                                        .setPositiveButton("知道了", null)
+                                        .create()
+                                        .show();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+                //id要放入card_relationship的id
+                String id="6";
+                SharedPreferences sp4 = getSharedPreferences("User", MODE_PRIVATE);
+                SharedPreferences.Editor editor4 = sp4.edit();
+                DeletecardrelationRequest getRequest4 = new DeletecardrelationRequest(id,responseListener4);
+                RequestQueue requestQueue4 = Volley.newRequestQueue(getApplicationContext());
+                requestQueue4.add(getRequest4);
+
+            }
+        });
     }
     public void fromSettingsToHome(){
         Intent intent = new Intent(Settings.this,Home.class);
@@ -164,6 +254,33 @@ public class Settings extends AppCompatActivity {
             super(Method.POST,  Getothercard_REQUEST_URL, listener, null);
             params = new HashMap<>();
             params.put("member_id", member_id);
+        }
+        @Override
+        public Map<String, String> getParams() {
+            return params;
+        }
+    }
+    public class AddcardrelationRequest extends StringRequest {
+        private static final String Addcardrelation_REQUEST_URL = "https://nccugo105306.000webhostapp.com/Addcardrelation.php";
+        private Map<String, String> params;
+        public AddcardrelationRequest(String member_id,String cardtype_name,Response.Listener<String> listener) {
+            super(Method.POST,  Addcardrelation_REQUEST_URL, listener, null);
+            params = new HashMap<>();
+            params.put("member_id", member_id);
+            params.put("cardtype_name", cardtype_name);
+        }
+        @Override
+        public Map<String, String> getParams() {
+            return params;
+        }
+    }
+    public class DeletecardrelationRequest extends StringRequest {
+        private static final String Deletecardrelation_REQUEST_URL = "https://nccugo105306.000webhostapp.com/Deletecardrelation.php";
+        private Map<String, String> params;
+        public DeletecardrelationRequest(String id , Response.Listener<String> listener) {
+            super(Method.POST,  Deletecardrelation_REQUEST_URL, listener, null);
+            params = new HashMap<>();
+            params.put("id", id);
         }
         @Override
         public Map<String, String> getParams() {
